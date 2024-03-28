271a3ac85fc04355d825d4190e17427e40aa03e6
Hunterdown1505 <133170617+Hunterdown1505@users.noreply.github.com>
2024-03-28 17:42:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let nomeDoHeroi = "InfinityHunt"
+let xpHeroi = 10001
+
+let mensagem
+
+if(xpHeroi <1000){
+    mensagem = "Ferro"
+}else if (xpHeroi >= 1001 && xpHeroi<=2000){
+    mensagem = "Bronze"
+}else if (xpHeroi >=2001 && xpHeroi <=5000){
+    mensagem = "Prata"
+}else if (xpHeroi >=5001 && xpHeroi <=7000){
+    mensagem = "Ouro"
+}else if (xpHeroi >=7001 && xpHeroi <=8000){
+    mensagem = "Platina"
+}else if (xpHeroi >=8001 && xpHeroi <= 9000){
+    mensagem = "Ascendente"
+}else if (xpHeroi >=9001 && xpHeroi <=10000){
+    mensagem = "Imortal"
+}else{
+    mensagem = "Radiante"
+}
+
+console.log("O herói " + nomeDoHeroi + " atingiu o ranking " + mensagem)
